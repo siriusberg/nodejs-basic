bfbd6ff3d1b0673e547a96624fc3855ec96421cb
Abu A'la <abualaanadwi1@gmail.com>
2023-04-30 14:15:41 +0700

    Bootcamp Exercise

---
+++ filesystem/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const {resolve} = require('path');
+
+
+const fileReadCallback = (error, data) => {
+    if(error){
+        console.log('Gagal membaca file');
+        return;
+    }
+    console.log(data);
+}
+
+fs.readFile(resolve(__dirname, 'notes.txt'), 'utf-8', fileReadCallback);
